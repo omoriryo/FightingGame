fb9a78ef83821a6b4d25d4a4ae1868714af9fa9d
omori ryo <ryo@omori-no-MacBook-Pro.local>
2016-12-22 22:20:47 +0900

    アニメーション動いた

--- Assets/Scripts/Action.cs
+++ Assets/Scripts/Action.cs
@@ -4,11 +4,9 @@ using UnityEngine;
 
 public class Action : MonoBehaviour {
 
-	// 速度
 	public Vector2 SPEED = new Vector2(0.1f, 0.1f);
 	// Use this for initialization
 	void Start () {
-
 	}
 
 	// Update is called once per frame
@@ -22,18 +20,30 @@ public class Action : MonoBehaviour {
 		// 現在位置をPositionに代入
 		Vector2 Position = transform.position;
 		// 左キーを押し続けていたら
-		if(Input.GetKey("left")){
+		if (Input.GetKey ("left")) {
 			// 代入したPositionに対して加算減算を行う
 			Position.x -= SPEED.x;
-		} else if(Input.GetKey("right")){ // 右キーを押し続けていたら
+			GetComponent<Animator> ().SetBool ("isMove", true);
+		} else if (Input.GetKey ("right")) { // 右キーを押し続けていたら
 			// 代入したPositionに対して加算減算を行う
 			Position.x += SPEED.x;
-		} else if(Input.GetKey("up")){ // 上キーを押し続けていたら
+			GetComponent<Animator> ().SetBool ("isMove", true);
+		} else if (Input.GetKey ("up")) { // 上キーを押し続けていたら
 			// 代入したPositionに対して加算減算を行う
-			Position.y += SPEED.y* 2;
-		} else if(Input.GetKey("down")){ // 下キーを押し続けていたら
+			Position.y += SPEED.y * 2;
+			GetComponent<Animator> ().SetBool ("isDown", true);
+		} else if (Input.GetKey ("down")) { // 下キーを押し続けていたら
 			// 代入したPositionに対して加算減算を行う
-			Position.y -= SPEED.y;
+			//Position.y -= SPEED.y;
+			GetComponent<Animator> ().SetBool ("isDown", true);
+		} else if (Input.GetKey ("p")) { // 下キーを押し続けていたら
+			// 代入したPositionに対して加算減算を行う
+			//Position.y -= SPEED.y;
+			GetComponent<Animator> ().SetTrigger("Punch");
+		}  
+		else {
+			GetComponent<Animator> ().SetBool ("isDown", false);
+			GetComponent<Animator> ().SetBool ("isMove", false);
 		}
 		// 現在の位置に加算減算を行ったPositionを代入する
 		transform.position = Position;
