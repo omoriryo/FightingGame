45f76697aa219f9d020e5f30e665380834e149fd
ryo omori <ryohmori@yairilab.net>
2017-01-01 09:20:54 +0900

    Sound Manager Create

---
+++ Assets/Scripts/Ohtello/GameManager.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameManager : MonoBehaviour {
+
+	public static GameManager instance = null;
+
+	//PlayerBase[] players;
+	public static bool isBlackTurn;
+	public static int Result = 0;
+	bool isEnd = true;
+	public static int TrunNum;
+	public AudioClip PutSound;
+
+	void Awake ()
+	{
+		//********** 開始 **********// 
+		//ゲーム開始時にGameManagerをinstanceに指定
+		if (instance == null) {
+			instance = this;
+			//このオブジェクト以外にGameManagerが存在する時
+		} else if (instance != this) {
+			//このオブジェクトを破壊する
+			Destroy(gameObject);
+		}
+		//シーン遷移時にこのオブジェクトを受け継ぐ
+		DontDestroyOnLoad(gameObject);
+		//********** 終了 **********// 
+	}
+
+	 //Update is called once per frame
+	void Update () {
+	} 
+}
---
+++ Assets/Scripts/Ohtello/Result.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Result : MonoBehaviour {
+	public AudioClip ResultSound;
+	public Sprite BlackWinSprite;
+	public Sprite WhiteWinSprite;
+	public Sprite DrawSprite;
+	SpriteRenderer ResultSprite;
+	// Use this for initialization
+	void Start ()
+	{
+		ResultSprite = gameObject.GetComponent<SpriteRenderer> ();
+		if (GameManager.Result == 0) {
+			ResultSprite.sprite = BlackWinSprite;
+		} else if(GameManager.Result == 1){
+			ResultSprite.sprite = WhiteWinSprite;
+		}else {
+			ResultSprite.sprite = DrawSprite;
+		}
+		StartCoroutine (ChangeScene ());
+	}
+
+	IEnumerator ChangeScene()
+	{
+		SoundManager.instance.BgmStop ();
+		SoundManager.instance.PlaySingle (ResultSound);
+		yield return new WaitForSeconds(8);
+		SoundManager.instance.BgmStart ();
+		Application.LoadLevel("Title");
+	}
+}
